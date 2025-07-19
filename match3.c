2038c8901d4f00a2769cd94ef67374f0906631e5
vaibhavkrmishr <mishra.vaibhavkumar9533@gmail.com>
2025-07-19 19:54:21 +0530

    Match 3

--- match3.c
+++ match3.c
@@ -11,12 +11,101 @@
 
 const char tile_chars[TILE_TYPES] ={'@','#','$','&','%'};
 char board[BORAD_SIZE][BORAD_SIZE];
+bool matched[BORAD_SIZE][BORAD_SIZE] ={0};
+float fall_offset[BORAD_SIZE][BORAD_SIZE] ={0};
+
+int score =00;
+
 Vector2 grid_origin;
 Texture2D background;
+Vector2 selected_tile = {-1,-1};
+float fall_speed =8.0f;
+float match_delay_timer =0.0f;
+const float match_delay_duration=0.3f;
+
+typedef enum{
+    State_Idle,
+    State_Animating,
+    state_match_delay
+} TileState;
+
+TileState tile_state;
 
 char randon_tile(){
     return tile_chars[rand() % TILE_TYPES];
 }
+
+void swap_tiles(int x1,int y1,int x2,int y2){
+    char temp = board[y1][x1];
+    board[y1][x1]=board[y2][x2];
+    board[y2][x2] =temp;
+}
+
+bool are_tiles_adjacent(Vector2 a, Vector2 b){
+    return (abs((int)a.x - (int)b.x)+abs((int)a.y - (int)b.y))==1;
+}
+
+bool find_matches(){
+    bool found = false;
+    for (int y=0;y<BORAD_SIZE;y++){
+        for (int x=0;x<BORAD_SIZE;x++){
+            matched[y][x]=false;
+        }
+    }
+    for (int y=0;y<BORAD_SIZE;y++){
+        for (int x=0;x<BORAD_SIZE -2;x++){
+            char t= board[y][x];
+            if (t==board[y][x+1] &&
+                t==board[y][x+2]){
+                    matched[y][x] =matched[y][x+1]=matched[y][x+2]=true;
+                //Update Score
+                score+=10;
+                found=true;
+                }
+        }
+    }
+    for (int x=0;x<BORAD_SIZE;x++){
+        for (int y=0;y<BORAD_SIZE -2;y++){
+            char t= board[y][x];
+            if (t==board[y+1][x] &&
+                t==board[y+2][x]){
+                    matched[y][x] =matched[y+1][x]=matched[y+2][x]=true;
+                //Update Score
+                score+=10;
+                found=true;
+                }
+        }
+    }
+
+    return found;
+
+}
+
+void resolve_matches(){
+    for (int x=0;x<BORAD_SIZE;x++){
+        int write_y=BORAD_SIZE-1;
+        for(int y= BORAD_SIZE -1;y>=0;y--){
+            if(!matched[y][x]){
+                if(y!=write_y){
+                    board[write_y][x]=board[y][x];
+                    fall_offset[write_y][x]=(write_y -y) * TILE_SIZE;
+                    board[y][x]=' ';
+                }
+                write_y--;
+            }
+        }
+
+        while( write_y>=0){
+            board[write_y][x]=randon_tile();
+            fall_offset[write_y][x]=(write_y +1) * TILE_SIZE;
+            write_y--;
+
+        }
+    }
+    tile_state=State_Animating;
+    
+}
+
 
 void init_board(){
     for(int y =0;y<BORAD_SIZE;y++){
@@ -33,6 +122,13 @@ void init_board(){
         (GetScreenHeight() - grid_height)/2
 
     };
+
+    if(find_matches()){
+        resolve_matches();
+    }
+    else{
+        tile_state=State_Idle;
+    }
 }
 
 int main(void){
@@ -47,10 +143,72 @@ int main(void){
 
     init_board();
     printf("Game started!\n");
+    Vector2 mouse ={0,0};
 
     while (!WindowShouldClose()){
 
         // Game Logic
+        mouse = GetMousePosition();
+        if (tile_state==State_Idle && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+            int x = (mouse.x - grid_origin.x) / TILE_SIZE;
+            int y = (mouse.y - grid_origin.y) / TILE_SIZE;
+            if (x>=0 && x< BORAD_SIZE && y>= 0 && y<BORAD_SIZE){
+                Vector2 current_tile =(Vector2){x,y};
+                if(selected_tile.x<0){
+                    selected_tile = current_tile;
+                }
+                else{
+                    if(are_tiles_adjacent(selected_tile,current_tile)){
+                        swap_tiles(selected_tile.x,selected_tile.y,current_tile.x,current_tile.y);
+                                if(find_matches()){
+                                    resolve_matches();
+                                 }
+                                 else{
+                                    swap_tiles(selected_tile.x,selected_tile.y,current_tile.x,current_tile.y);
+                                 }
+                                 selected_tile= (Vector2){-1,-1};
+
+                    }
+                }
+            }
+        }
+if(find_matches()){
+            resolve_matches();
+        }
+
+        if(tile_state==State_Animating){
+            bool still_animating =false;
+
+        for(int y =0;y<BORAD_SIZE;y++){
+            for (int x =0;x<BORAD_SIZE;x++){
+                if(fall_offset[y][x]>0){
+                    fall_offset[y][x]-=fall_speed;
+                    if(fall_offset[y][x]<0){
+                        fall_offset[y][x]=0;
+                    }else{
+                        still_animating=true;
+                    }
+                }
+            }
+        }
+        if(!still_animating){
+            tile_state=state_match_delay;
+            match_delay_timer=match_delay_duration;
+        }
+        }
+        if(tile_state==state_match_delay){
+            match_delay_timer-=GetFrameTime();
+            if (match_delay_timer<=0.0f){
+                if(find_matches()){
+                    resolve_matches();
+
+                }
+                else{
+                    tile_state = State_Idle;
+                }
+            }
+        }
+
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -76,23 +234,36 @@ int main(void){
 
             DrawRectangleLinesEx(rect,1,DARKGRAY);
 
+            if(board[y][x]!=' '){
             DrawTextEx(
                 GetFontDefault(),
 						TextFormat("%c", board[y][x]),
 						(Vector2) {
 							rect.x + 12,
-							rect.y + 8 
+							rect.y + 8 - fall_offset[y][x]
 						},
 						20,
-						1, WHITE
+						1, 
+                        matched[y][x]?GREEN:WHITE
             );
         }
+        }
     }
+    // draw selected tiles
+    if (selected_tile.x>=0){
+        DrawRectangleLinesEx((Rectangle){
+            grid_origin.x + (selected_tile.x * TILE_SIZE),
+            grid_origin.y + (selected_tile.y * TILE_SIZE),
+            TILE_SIZE, TILE_SIZE
+        },2, YELLOW);
+    }
+        
+        DrawText(TextFormat("Score: %d",score),20,20,24,RED);
 
         EndDrawing();
 
     }
-UnloadTexture(background);
+    UnloadTexture(background);
 
     CloseWindow();
 
